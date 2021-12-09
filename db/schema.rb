c26bb1a2634839d6bef4a89504175339b14d665d
Sierra Tucker <sierra.tucker121@gmail.com>
2021-12-09 15:55:25 -0700

    Create migration for applications

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_13_001717) do
+ActiveRecord::Schema.define(version: 2021_12_09_224939) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "applications", force: :cascade do |t|
+    t.string "applicant_name"
+    t.string "address"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pets", force: :cascade do |t|
     t.boolean "adoptable"
---
+++ spec/features/applications/show_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe 'Application Show Page' do
+  describe 'When visiting the applicaiton show page' do
+    before(:each) do
+    end
+
+    it 'displays the name of applicant' do
+    end
+
+    it 'displays the full address of applicant' do
+    end
+
+    it 'has a description for why the applicant says they are a good home' do
+    end
+
+    it 'lists the names of all pets that the application is for' do
+    end
+
+    it 'links each pet on the application to its show page' do
+    end
+
+    it 'displays the application status' do
+    end
+  end
+end
