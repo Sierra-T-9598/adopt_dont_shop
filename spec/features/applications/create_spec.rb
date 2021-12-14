2c3d5f906248c9bcd3f6bb5059d42e69cffd24d8
Sierra Tucker <sierra.tucker121@gmail.com>
2021-12-14 13:16:33 -1000

    Add within blocks for testing new application pages

--- spec/features/applications/create_spec.rb
+++ spec/features/applications/create_spec.rb
@@ -6,33 +6,40 @@ RSpec.describe 'New Application Page', type: :feature do
       describe 'the application new' do
         it 'renders the new form' do
           visit '/applications/new'
-
           expect(page).to have_content('New Application')
-          expect(find('form')).to have_content('Name')
-          expect(find('form')).to have_content('Address')
-          expect(find('form')).to have_content('City')
-          expect(find('form')).to have_content('State')
-          expect(find('form')).to have_content('Zip')
+
+          within('#new_app') do
+            expect(find('form')).to have_content('Name')
+            expect(find('form')).to have_content('Address')
+            expect(find('form')).to have_content('City')
+            expect(find('form')).to have_content('State')
+            expect(find('form')).to have_content('Zip')
+          end
         end
 
       describe 'the application create' do
         it 'can create a new application and redirect to the new apps show page' do
           visit '/applications/new'
 
-          fill_in(:applicant_name, with: 'George')
-          fill_in(:street_address, with: '34798 Kendall Way')
-          fill_in(:city, with: 'Westminister')
-          fill_in(:state, with: 'Colorado')
-          fill_in(:zip_code, with: '80944')
+          within('#new_app') do
+            fill_in(:applicant_name, with: 'George')
+            fill_in(:street_address, with: '34798 Kendall Way')
+            fill_in(:city, with: 'Westminister')
+            fill_in(:state, with: 'Colorado')
+            fill_in(:zip_code, with: '80944')
 
-          click_button('Submit')
+            click_button('Submit')
+          end
 
           expect(current_path).to eq("/applications/#{Application.last.id}")
-          expect(page).to have_content("George")
-          expect(page).to have_content('34798 Kendall Way')
-          expect(page).to have_content('Westminister')
-          expect(page).to have_content('Colorado')
-          expect(page).to have_content('80944')
+
+          within('#application_info') do
+            expect(page).to have_content("George")
+            expect(page).to have_content('34798 Kendall Way')
+            expect(page).to have_content('Westminister')
+            expect(page).to have_content('Colorado')
+            expect(page).to have_content('80944')
+          end
         end
       end
 
@@ -40,7 +47,9 @@ RSpec.describe 'New Application Page', type: :feature do
         it 'flashes an error message that fields are incomplete and stays on same page' do
           visit '/applications/new'
 
-          click_button('Submit')
+          within('#new_app') do
+            click_button('Submit')
+          end
 
           expect(page).to have_content("Error: Applicant name can't be blank, Street address can't be blank, City can't be blank, State can't be blank, Zip code can't be blank")
         end
